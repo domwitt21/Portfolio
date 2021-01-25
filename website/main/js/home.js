721309dd1e78a8fccf8af18a87e78ace7d91cd9f
domwitt21 <60725257+domwitt21@users.noreply.github.com>
2021-01-25 18:30:38 -0500

    Portfolio
    Portfolio

---
+++ website/main/js/home.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function mySecond() {
+    var element = document.body;
+    element.classList.toggle("dark-mode")
+}
